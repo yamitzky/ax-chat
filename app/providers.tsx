18f27535b37943dd7c8da8e214998740004b6b73
Mitsuki Ogasahara <negiga@gmail.com>
2025-12-01 21:16:57 +0900

    remove unused tanstack

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,14 @@
-'use client'
+"use client"
 
-import { ChatRepositoryProvider, DexieChatRepository } from '@/features/chat/repositories/chat-repository'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState } from 'react'
+import {
+  ChatRepositoryProvider,
+  DexieChatRepository,
+} from "@/features/chat/repositories/chat-repository"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChatRepositoryProvider value={new DexieChatRepository()}>
-        {children}
-      </ChatRepositoryProvider>
-    </QueryClientProvider>
+    <ChatRepositoryProvider value={new DexieChatRepository()}>
+      {children}
+    </ChatRepositoryProvider>
   )
 }
